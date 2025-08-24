072b60037cb7bfc1e2a73b6a6e3224b81948b9b2
Lubos Sytensky <lsytensky@csas.cz>
2025-08-24 14:44:49 +0200

    Removed unused imports

--- app/src/main/java/cz/yogaboy/tv/MainActivity.kt
+++ app/src/main/java/cz/yogaboy/tv/MainActivity.kt
@@ -1,20 +1,12 @@
 package cz.yogaboy.tv
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.lifecycleScope
 import cz.yogaboy.app.RootNavGraph
 import cz.yogaboy.data.marketdata.MarketDataRepository
@@ -37,16 +29,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             TradingViewTheme {
                 RootNavGraph()
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Box(
-//                        modifier = Modifier
-//                            .fillMaxSize()
-//                            .padding(innerPadding),
-//                        contentAlignment = Alignment.Center
-//                    ) {
-//                        Text(uiText)
-//                    }
-//                }
             }
         }
     }
