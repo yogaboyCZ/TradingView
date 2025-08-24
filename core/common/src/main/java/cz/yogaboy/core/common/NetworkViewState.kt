dba7d13281744e1b2f6aca05f8aafd930a9ca412
Lubos Sytensky <lsytensky@csas.cz>
2025-08-24 17:17:18 +0200

    Add module core:common

---
+++ core/common/src/main/java/cz/yogaboy/core/common/NetworkViewState.kt
@@ -0,0 +1,7 @@
+package cz.yogaboy.core.common
+
+sealed class NetworkViewState<T> {
+    data class Success<T>(val value: T) : NetworkViewState<T>()
+    data class Error<T>(val throwable: Throwable, val uiErrorMessage: String? = null) : NetworkViewState<T>()
+    data class Loading<T>(val text: String = "", val description: String = "") : NetworkViewState<T>()
+}
