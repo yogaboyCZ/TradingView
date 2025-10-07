928cf67923fda23026492e0dada31093df7152e8
Lubos <sytensky@gmail.com>
2025-10-07 16:07:03 +0200

    conventional DSL to core:common module

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -1,39 +1,15 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions.freeCompilerArgs.add("-Xcontext-parameters")
+}
+
 plugins {
-    alias(libs.plugins.android.library)
+    id("tradingview.android-library")
 }
 
 android {
     namespace = "cz.yogaboy.core.common"
-    compileSdk {
-        version = release(36)
-    }
-
-    defaultConfig {
-        minSdk = 33
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-}
-
-kotlin {
-    compilerOptions {
-        freeCompilerArgs.add("-Xcontext-parameters")
-    }
 }
 
 dependencies {
